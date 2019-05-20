b6ad54072bd0ea4cbe154a6de2f2f4cf6e470b94
Wonho ha <haho013039@gmail.com>
2019-05-20 20:58:46 +0900

    Refactoring

--- notice.py
+++ notice.py
@@ -10,6 +10,9 @@ NEWS_TABLE_XPATH = """//*[@id="hnmain"]/tbody/tr[3]/td/table/tbody"""
 HTML_FORMAT = "<li><a href='{url}'>{title}</a><li>"
 history = []
 
+dr = create_webdriver()
+l = get_news(dr, [BEST_NEWS_URL, ACTIVE_NEWS_URL])
+
 
 def create_webdriver(path="./chromedriver.exe", wait=3):
     driver = webdriver.Chrome(path)
@@ -17,41 +20,53 @@ def create_webdriver(path="./chromedriver.exe", wait=3):
     return driver
 
 
+def get_news(driver, links):
+    news = dict()
+    for link in links:
+        news.update(get_news_from_link(driver, link))
+    return news
+
+
+def get_news_from_link(driver, link):
+    titles, article_links = get_title_and_links(driver, link)
+    return get_processed_links(titles, article_links)
+
+
+def get_title_and_links(web_driver, link):
+    web_driver.get(link)
+    body = web_driver.find_element_by_xpath(NEWS_TABLE_XPATH)
+    titles = body.find_elements_by_class_name("athing")
+    links = body.find_elements_by_class_name("storylink")
+    return titles, links
+
+
+def get_processed_links(titles, article_links):
+    links = dict()
+    for i in range(len(titles)):
+        title = processing_title(titles[i].text)
+        link = get_link_from_eliment(article_links[i])
+        if is_new_link(link):
+            title = change_new_title(title)
+        history.append(link)
+        links[title] = link
+    return links
+
+
 def processing_title(title):
     words = title.split()[1:-1]
     return reduce(lambda x, y: x + " " + y, words)
 
 
-def get_best_news(driver):
-    links = dict()
-    driver.get(BEST_NEWS_URL)
-    body = driver.find_element_by_xpath(NEWS_TABLE_XPATH)
-    titles = body.find_elements_by_class_name("athing")
-    article_links = body.find_elements_by_class_name("storylink")
-    for i in range(len(titles)):
-        title = processing_title(titles[i].text)
-        link = article_links[i].get_attribute('href')
-        if not link in history:
-            title = "[NEW]" + title
-        history.append(link)
-        links[title] = link
-    return links
+def get_link_from_eliment(el):
+    return el.get_attribute('href')
 
 
-def get_active_news(driver):
-    links = dict()
-    driver.get(ACTIVE_NEWS_URL)
-    body = driver.find_element_by_xpath(NEWS_TABLE_XPATH)
-    titles = body.find_elements_by_class_name("athing")
-    article_links = body.find_elements_by_class_name("storylink")
-    for i in range(len(titles)):
-        title = processing_title(titles[i].text)
-        link = article_links[i].get_attribute('href')
-        if not link in history:
-            title = "[NEW]" + title
-        history.append(link)
-        links[title] = link
-    return links
+def is_new_link(link):
+    return not link in history
+
+
+def change_new_title(title_text):
+    return "[NEW]" + title_text
 
 
 def dict_to_html(articles):
@@ -63,9 +78,10 @@ def dict_to_html(articles):
     return "<ol>{html}</ol>".format(html=result_html)
 
 
+## This is WebServer
+
+
 app = Sanic()
-dr = create_webdriver()
-l = get_best_news(dr)
 
 
 @app.route('/')
@@ -76,8 +92,7 @@ async def main_news(request):
 @app.route('/reload')
 async def reload_news(request):
     global l
-    l = get_best_news(dr)
-    l.update(get_active_news(dr))
+    l = get_news(dr, [BEST_NEWS_URL, ACTIVE_NEWS_URL])
     return json(
         {"reload news": "seccess"}
     )
